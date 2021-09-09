0dbdb3633996919b384cbd848525704260d92c81
Jonas Lindell <Carljonaslindell@gmail.com>
2021-09-09 12:20:39 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace github_actions_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
